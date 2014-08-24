c16031db73f32eb0d411458dd1f0bb4642ed4a8d
alejandro <alejandro.osorio@vtex.com.br>
2014-08-24 14:08:06 -0300

    improving the comments of the function

--- cachematrix.R
+++ cachematrix.R
@@ -5,12 +5,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
+    ## function that sets the value of the matrix
+    ## if the value of the matrix change the inverse must be recalculated
+    ## then the cache must be cleaned
     set <- function(y) {
         x <<- y
         m <<- NULL
     }
+    ## get the value of the matrix
     get <- function() x
+    ## set the value of the inverse of the matrix
     setsolve <- function(solve) m <<- solve
+    ## get the value of the inverse of the matrix
     getsolve <- function() m
     list(set = set, get = get,
          setsolve = setsolve,
@@ -22,7 +28,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Function that gets the inverse of a matrix from the cache version if possible
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Returns the inverse of the matrix
     m <- x$getsolve()
     if(!is.null(m)) {
         message("getting cached inverse matrix")
@@ -30,6 +36,7 @@ cacheSolve <- function(x, ...) {
     }
     data <- x$get()
     m <- solve(data, ...)
+    ## If the inverse is not cached then calculates the inverse and sets the cache value
     x$setsolve(m)
     m
 }
