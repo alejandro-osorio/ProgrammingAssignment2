2631d43343eaa2582eeb7d02b2b62c68e53a64ab
alejandro <alejandro.osorio@vtex.com.br>
2014-08-24 13:47:14 -0300

    adding the function to calculate a cached inversion of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Fucntions to get the inverse of a matrix with a cached value
 
-## Write a short comment describing this function
+## Function that create an special object that contains the value of a matrix and expose 
+#  the functionality to calculate the solve just if the value of the matrix has changed
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## Function that gets the inverse of a matrix from the cache version if possible
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached inverse matrix")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
